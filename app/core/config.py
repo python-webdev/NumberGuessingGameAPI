10a066c2b504269673688215e5b3b2c3d874f384
Murod Akhmedov <python_webdev@outlook.com>
2026-03-24 19:45:26 +0000

    chore: add database schema

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    MAX_ATTEMPTS: int = 10
+    NUMBER_RANGE_MIN: int = 1
+    NUMBER_RANGE_MAX: int = 100
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
